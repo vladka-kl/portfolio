aa875496ed9f05e066e18ce6d6ec1584dd5be488
Vladyslava Klovak <vladaklovak@gmail.com>
2023-03-08 15:54:51 +0100

    Create folders for components, create Siderbar component

---
+++ components/Sidebar/index.jsx
@@ -0,0 +1,8 @@
+import classes from "./classes.module.css";
+import react from "react";
+
+const Sidebar = () => {
+  return <div className={classes.sideBarContainer}></div>;
+};
+
+export default Sidebar;
